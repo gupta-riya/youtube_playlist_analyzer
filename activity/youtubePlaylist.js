f723a30c8cb96114b5e6e1244c9289b0760382e0
Riya Gupta <61061825+gupta-riya@users.noreply.github.com>
2021-04-15 00:16:48 +0530

    added comments

--- activity/youtubePlaylist.js
+++ activity/youtubePlaylist.js
@@ -4,46 +4,41 @@ let fs = require('fs');
 // no of videos
 // views done
 // watch time-> get
-// list of videos -> in an excel
-// initial page data get
-// handle -> loader
+// getting all the videos available
 
 
-console.log("before");
+
 (async function () {
     try {
+        //browser launched
         let browserInstance = await puppeteer.launch({
             headless: false,
             defaultViewport: null,
             args: ["--start-maximized"]
 
         });
+
         let newPage = await browserInstance.newPage();
         await newPage.goto("https://www.youtube.com/playlist?list=PLRBp0Fe2GpgnIh0AiYKh7o7HnYAej-5ph");
 
         //evaluate
         let staticInfo = await newPage.evaluate(consoleFn);
+
+        //video Count
         let videoCount = Number(staticInfo[0].split(" ")[0])
         console.log(videoCount);
         console.log(staticInfo[1]);
 
-        let currCount = await scrolltoBottom(newPage,"#video-title");
-        while(videoCount-50 > currCount)
-        {
-            currCount = await scrolltoBottom(newPage,"#video-title");
+        // call to scrolltoBottom function
+        let currCount = await scrolltoBottom(newPage, "#video-title");
+        while (videoCount - 50 > currCount) {
+            currCount = await scrolltoBottom(newPage, "#video-title");
         }
 
+        // call a functio to return name and duration of the video mentioned in the playlist
         let NameDurArr = await newPage.evaluate(getStats, "span.style-scope.ytd-thumbnail-overlay-time-status-renderer", "#video-title");
         console.table(NameDurArr);
-        //get videos duration
 
-
-        //initial data -> data extract
-        //span.style-scope.ytd-thumbnail-overlay-time-status-renderer
-        //#video-title -> video title
-
-        //scroll-> loader wait
-        // dara extract -> next set of videos
 
     }
     catch (err) {
@@ -51,7 +46,7 @@ console.log("before");
     }
 })();
 
-
+// function to get number of videos and number of views
 function consoleFn() {
     let arr = document.querySelectorAll("#stats.style-scope .ytd-playlist-sidebar-primary-info-renderer");
     let newArr = [];
@@ -60,24 +55,21 @@ function consoleFn() {
     return newArr;
 }
 
-//function to scroll at the bottom
-async function scrolltoBottom(newPage,titleSelector)
-{
-    function getLengthConsoleFn(titleSelector)
-    {
-        window.scrollBy(0,window.innerHeight);
+//function to scroll at the bottom till all the videos are rendered on the screen
+async function scrolltoBottom(newPage, titleSelector) {
+    function getLengthConsoleFn(titleSelector) {
+        window.scrollBy(0, window.innerHeight);
         let titleElemArr = document.querySelectorAll(titleSelector);
-        console.log("titleLength ",titleElemArr.length);
-        if(titleElemArr.length==899)
-        {
+        console.log("titleLength ", titleElemArr.length);
+        if (titleElemArr.length == 899) {
             console.log(titleElemArr);
         }
         return titleElemArr.length;
     }
-    return newPage.evaluate(getLengthConsoleFn,titleSelector);
+    return newPage.evaluate(getLengthConsoleFn, titleSelector);
 }
 
-
+// to get name and duration of every video
 
 function getStats(durationSelector, titleSelector) {
     let dElemArr = document.querySelectorAll(durationSelector);
